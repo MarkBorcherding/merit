29a40656de7ceefd4576c154616ee59b06d0d588
Mark Borcherding <markborcherding@gmail.com>
2012-08-06 09:37:39 -0500

    Mongo mapper and Mongo changes to follow new pattern. PROBABLY BROKEN STILL
    I just moved the mongo mapper and mongoid model additions to their own module like
    the ActiveRecord stuff. I know it doesn't work, but I don't have the skills to keep
    it current just yet. Once AR is working, I'll try to go back and make the changes to
    the Mongo bits.

--- lib/merit/models/mongo_mapper/model_additions.rb
+++ lib/merit/models/mongo_mapper/model_additions.rb
@@ -1,30 +1,32 @@
 module Merit
-  extend ActiveSupport::Concern
+  module ModelAdditions
+    extend ActiveSupport::Concern
 
-  module ClassMethods
-    def has_merit(options = {})
-      plugin Merit
-      key :sash_id, String
-      key :points, Integer, :default => 0
-      key :level, Integer, :default => 0
+    module ClassMethods
+      def has_merit(options = {})
+        plugin Merit
+        key :sash_id, String
+        key :points, Integer, :default => 0
+        key :level, Integer, :default => 0
+      end
     end
-  end
 
-  def award_points(points)
-    create_sash_if_none
-    sash.awarded_points.create :points => points
-  end
+    def award_points(points)
+      create_sash_if_none
+      sash.awarded_points.create :points => points
+    end
 
-  def badges
-    create_sash_if_none
-    sash.badge_ids.collect{|b_id| Badge.find(b_id) }
-  end
+    def badges
+      create_sash_if_none
+      sash.badge_ids.collect{|b_id| Badge.find(b_id) }
+    end
 
-  # Create sash if doesn't have
-  def create_sash_if_none
-    if self.sash.blank?
-      self.sash = Sash.create
-      self.save(:validate => false)
+    # Create sash if doesn't have
+    def create_sash_if_none
+      if self.sash.blank?
+        self.sash = Sash.create
+        self.save(:validate => false)
+      end
     end
   end
 end
--- lib/merit/models/mongoid/model_additions.rb
+++ lib/merit/models/mongoid/model_additions.rb
@@ -1,35 +1,38 @@
 module Merit
-  extend ActiveSupport::Concern
+  module ModelAdditions
+    extend ActiveSupport::Concern
 
-  module ClassMethods
-    def has_merit(options = {})
+    module ClassMethods
+      def has_merit(options = {})
         field :sash_id
         field :points, :type => Integer, :default => 0
         field :level, :type => Integer, :default => 0
+      end
+
+      def find_by_id(id)
+        here(:_id => id).first
+      end
     end
 
-    def find_by_id(id)
-      here(:_id => id).first
+    def award_points(points)
+      create_sash_if_none
+      sash.awarded_points.create :points => points
     end
-  end
 
-  def award_points(points)
-    create_sash_if_none
-    sash.awarded_points.create :points => points
-  end
+    def badges
+      create_sash_if_none
+      sash.badge_ids.collect{|b_id| Badge.find(b_id) }
+    end
 
-  def badges
-    create_sash_if_none
-    sash.badge_ids.collect{|b_id| Badge.find(b_id) }
-  end
-
-  # Create sash if doesn't have
-  def create_sash_if_none
-    if self.sash.blank?
-      self.sash = Sash.create
-      self.save(:validate => false)
+    # Create sash if doesn't have
+    def create_sash_if_none
+      if self.sash.blank?
+        self.sash = Sash.create
+        self.save(:validate => false)
+      end
     end
   end
 end
 
 Mongoid::Document.send :include, Merit
+
